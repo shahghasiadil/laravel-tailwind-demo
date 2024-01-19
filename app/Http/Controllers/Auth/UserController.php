cf49dc02f70250d6e648ce51d00903f943bfd002
Omar Djecevic <djecevic.omar@gmail.com>
2024-01-19 17:39:27 +0100

    factory, migration and seeder for users table

---
+++ app/Http/Controllers/Auth/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    //
+}
