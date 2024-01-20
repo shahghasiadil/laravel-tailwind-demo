70fe1008e32ed60d9e6690aa383ab635fd9d27eb
Omar Djecevic <djecevic.omar@gmail.com>
2024-01-20 15:00:29 +0100

    categories table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(DepartmentSeeder::class);
         $this->call(UserDepartmentShiftSeeder::class);
+        $this->call(CategorySeeder::class);
     }
 }
