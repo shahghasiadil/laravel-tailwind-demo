bad5de3556c7413dfa3384cfaeccad4ad8145559
Shahghasi Adil <shaghasy555@gmail.com>
2024-01-27 09:29:07 +0430

    feat: categories with latest data

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,7 +13,7 @@ class CategoryController extends Controller
     public function index()
     {
         return view('categories.index', [
-            'categories' => Category::all(),
+            'categories' => Category::latest()->get(),
         ]);
     }
 
