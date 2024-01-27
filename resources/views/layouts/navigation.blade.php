c6270c5b14738130c3a7b1bd25ca8a2ff11cb30c
Shahghasi Adil <shaghasy555@gmail.com>
2024-01-27 17:29:05 +0430

    feat: menu nav for tickets added

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -34,6 +34,12 @@
                     </x-nav-link>
                 </div>
 
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('tickets.index')" :active="request()->routeIs('tickets.*')">
+                        {{ __('Tickets') }}
+                    </x-nav-link>
+                </div>
+
                 {{-- <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
 
                     <div class="flex">
