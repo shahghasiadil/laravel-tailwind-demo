d15dcdb571c52727fba4e5de2cdeed7997836ce4
Shahghasi Adil <shaghasy555@gmail.com>
2024-01-27 17:20:08 +0430

    feat: user finished

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -96,4 +96,74 @@
 
 
 </body>
-</html>
+</html> --}}
+
+<x-app-layout>
+    <x-slot name="title">
+        {{ __('messages.edit_user') }}
+    </x-slot>
+
+    <div class="min-h-screen flex flex-col sm:justify-center items-center sm:pt-0 bg-gray-100 dark:bg-gray-900">
+        <div>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ __('messages.edit_user') }}
+            </h2>
+        </div>
+
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <form action="{{ route('users.update', $user) }}" method="POST" class="max-w-sm mx-auto mt-6 space-y-6">
+                @csrf
+                @method('PUT')
+                <!-- Name -->
+                <div>
+                    <x-input-label for="name" :value="__('Name')" />
+                    <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name', $user->name)"
+                        required autofocus autocomplete="name" />
+                    <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                </div>
+
+                <!-- Email Address -->
+                <div class="mt-4">
+                    <x-input-label for="email" :value="__('Email')" />
+                    <x-text-input id="email" class="block mt-1 w-full" type="email" name="email"
+                        :value="old('email', $user->email)" required autocomplete="username" />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
+
+                <!-- Password -->
+                <div class="mt-4">
+                    <x-input-label for="password" :value="__('Password')" />
+
+                    <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                        autocomplete="new-password" />
+
+                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                </div>
+
+                <!-- Confirm Password -->
+                <div class="mt-4">
+                    <x-input-label for="dob" :value="__('messages.dob')" />
+
+                    <x-text-input id="dob" class="block mt-1 w-full" type="date" name="dob" required
+                        autocomplete="dob" :value="old('dob', $user->dob)" />
+
+                    <x-input-error :messages="$errors->get('dob')" class="mt-2" />
+                </div>
+
+                <div class="mt-4">
+                    <x-input-label for="identification_number" :value="__('messages.identification_number')" />
+
+                    <x-text-input id="identification_number" class="block mt-1 w-full" type="text"
+                        name="identification_number" required autocomplete="identification_number" :value="old('identification_number', $user->identification_no)" />
+
+                    <x-input-error :messages="$errors->get('identification_no')" class="mt-2" />
+                </div>
+                <div class="flex items-center gap-4">
+                    <x-primary-button>{{ __('messages.update') }}</x-primary-button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+
+</x-app-layout>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+{{-- <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Users list') }}
@@ -9,16 +9,156 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <x-bladewind::table :data="$users->toArray()['data']"
-                        :action_icons="$action_icons"
-                        :column_aliases="$captions" 
-                        exclude_columns="id, updated_at, deleted_at, created_at" 
-                        striped="true" divider="thin" has_shadow="true" compact="true" actions_title="{{ __('Actions') }}" 
-                    />
-                        
+                    <x-bladewind::table :data="$users->toArray()['data']" :action_icons="$action_icons" :column_aliases="$captions"
+                        exclude_columns="id, updated_at, deleted_at, created_at" striped="true" divider="thin"
+                        has_shadow="true" compact="true" actions_title="{{ __('Actions') }}" />
+
                     {{ $users->links() }}
                 </div>
             </div>
         </div>
     </div>
+</x-app-layout> --}}
+
+<x-app-layout>
+    <x-slot name="title">
+        {{ __('Users list') }}
+    </x-slot>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Users list') }}
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg py-12 px-4">
+                <div class="relative overflow-x-auto  sm:rounded-lg">
+                    <div class="flex justify-end gap-2 px-2 py-4">
+                        <a href="{{ route('users.create') }}"
+                            class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">{{ __('messages.create_new_user') }}</a>
+
+                    </div>
+
+                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    #
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    {{ __('messages.name') }}
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    {{ __('messages.email') }}
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    {{ __('messages.identification_number') }}
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    {{ __('messages.dob') }}
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    {{ __('messages.locale') }}
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    @lang('messages.action')
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                                <tr
+                                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                    <td class="px-6 py-4">
+                                        {{ $loop->iteration }}
+                                    </td>
+                                    <th scope="row"
+                                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        {{ $user->name }}
+                                    </th>
+                                    <th scope="row"
+                                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        {{ $user->email }}
+                                    </th>
+                                    <th scope="row"
+                                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        {{ $user->identification_no }}
+                                    </th>
+                                    <th scope="row"
+                                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        {{ $user->dob }}
+                                    </th>
+                                    <th scope="row"
+                                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        {{ $user->locale }}
+                                    </th>
+                                    <td class="px-6 py-4 flex flex-column gap-2">
+                                        <a href="{{ route('users.edit', $user->id) }}"
+                                            class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                            </svg>
+
+                                        </a>
+
+                                        <span>
+                                            <form action="{{ route('users.destroy', $user->id) }}" method="POST"
+                                                class="delete__user">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="action-button" title="Delete">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                        class="w-6 h-6 text-red-500">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                                    </svg>
+                                                </button>
+
+                                            </form>
+                                        </span>
+
+
+                                    </td>
+                                </tr>
+                            @endforeach
+
+                        </tbody>
+                    </table>
+                    <div class="pagination-container py-4">
+                        {{ $users->links() }}
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    </div>
+
+    @push('scripts')
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                document.querySelectorAll('.delete__user').forEach(function(form) {
+                    form.addEventListener('submit', function(event) {
+                        event.preventDefault();
+
+                        const currentForm = this;
+                        Swal.fire({
+                            title: `Are you sure you want to delete this User?`,
+                            text: "If you delete this, it will be gone forever.",
+                            icon: "warning",
+                            buttons: true,
+                            dangerMode: true,
+                        }).then((willDelete) => {
+                            if (willDelete) {
+                                currentForm.submit();
+                            }
+                        });
+                    });
+                });
+            });
+        </script>
+    @endpush
 </x-app-layout>
