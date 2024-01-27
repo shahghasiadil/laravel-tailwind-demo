d9e42a5c5dd00b61250b09c11cf003193be1fe59
Shahghasi Adil <shaghasy555@gmail.com>
2024-01-27 17:23:33 +0430

    feat: pagination added to categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,7 +13,7 @@ class CategoryController extends Controller
     public function index()
     {
         return view('categories.index', [
-            'categories' => Category::latest()->get(),
+            'categories' => Category::latest()->paginate(10),
         ]);
     }
 
