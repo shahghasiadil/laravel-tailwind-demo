cf0a7adfc7169d034eeb8ad09c9585e84dc87580
Shahghasi Adil <shaghasy555@gmail.com>
2024-01-28 07:38:23 +0430

    feat: department create

--- resources/views/departments/index.blade.php
+++ resources/views/departments/index.blade.php
@@ -16,7 +16,7 @@
                             class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">{{ __('messages.create_new_department') }}</a>
 
                     </div>
-
+                    {{--
                     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
@@ -78,39 +78,53 @@
                             @endforeach
 
                         </tbody>
-                    </table>
+                    </table> --}}
+
+                    <x-table :data="$departments->toArray()['data']" :action_icons="$action_icons" :column_aliases="$captions"
+                        exclude_columns="id, updated_at, deleted_at, created_at" striped="true" divider="thin"
+                        has_shadow="true" compact="true" actions_title="{{ __('Actions') }}" />
+
+
                     <div class="pagination-container py-4">
                         {{ $departments->links() }}
                     </div>
+                    <x-bladewind::modal name="delete-department" type="error" title="Confirm Department Deletion"
+                        ok_button_action="executDelete()">
+                        Are you really sure you want to delete <b class="title"></b>?
+                        This action cannot be reversed.
+
+                        <x-text-input id="id" type="hidden" />
+                    </x-bladewind::modal>
                 </div>
 
             </div>
         </div>
     </div>
     </div>
+    <script>
+        deleteDep = (id, title) => {
+            showModal('delete-department')
 
-    @push('scripts')
-        <script>
-            document.addEventListener('DOMContentLoaded', function() {
-                document.querySelectorAll('.delete__dep').forEach(function(form) {
-                    form.addEventListener('submit', function(event) {
-                        event.preventDefault();
+            domEl('#id').value = id;
+            domEl('.bw-delete-department .title').innerText = title;
+        };
 
-                        const currentForm = this;
-                        Swal.fire({
-                            title: `Are you sure you want to delete this Department?`,
-                            text: "If you delete this, it will be gone forever.",
-                            icon: "warning",
-                            buttons: true,
-                            dangerMode: true,
-                        }).then((willDelete) => {
-                            if (willDelete) {
-                                currentForm.submit();
-                            }
-                        });
-                    });
-                });
-            });
-        </script>
-    @endpush
+        executDelete = () => {
+            const id = document.getElementById('id').value;
+            if (id) {
+                fetch(`/departments/${id}`, {
+                        method: 'delete',
+                        headers: {
+                            'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                        },
+                    })
+
+                    .then(data => {
+                        window.location.reload();
+                    })
+                    .catch(error => console.error('Error:', error));
+            }
+        };
+    </script>
+
 </x-app-layout>
